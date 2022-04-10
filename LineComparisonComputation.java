879e5fd1b04576d022db45afa6c4ee16f904aa4c
Padmajase <padmajapawar7@gmail.com>
2022-04-09 23:37:13 -0700

    Added : Line Comparison Computation program UC1

--- LineComparisonComputation.java
+++ LineComparisonComputation.java
@@ -1,5 +1,18 @@
-class LineComparisonComputation {
+import java.util.Scanner;
+import java.lang.Math;
+
+class UC1 {
 	public static void main (String args[]) {
 		System.out.println("Welcome !");
+		
+		System.out.println("Enter values of  x1 y1 x2 y2");
+		Scanner sc = new Scanner(System.in);
+		int x1 = sc.nextInt();	
+		int y1 = sc.nextInt();
+		int x2 = sc.nextInt();
+		int y2 = sc.nextInt();	
+
+		double length = Math.sqrt( Math.pow((x2 - x1), 2) + Math.pow( (y2- y1), 2));
+		System.out.println(length);
 	}
 }
