a65a5da014229c4070b3f4e71ed8fb8fba83bdee
Padmajase <padmajapawar7@gmail.com>
2022-04-09 23:28:25 -0700

    Added : Line Comparison Computation program

---
+++ LineComparisonComputation.java
@@ -0,0 +1,7 @@
+// Start with Displaying Welcome to Line Comparison Computation Program on Master Branch
+
+class LineComparisonComputation {
+	public static void main (String args[]) {
+		System.out.println("Welcome !");
+	}
+}
